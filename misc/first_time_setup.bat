5d861b9b0a1c30b4d26033c3e2c7a1da3a54a3be
Chris LaBauve <jiminychris@gmail.com>
2024-10-24 08:08:41 -0500

    Fix first time setup.

--- misc/first_time_setup.bat
+++ misc/first_time_setup.bat
@@ -16,7 +16,7 @@ echo sLinkFile = desktop ^& "\%PROJECT_NAME% Shell.lnk" >> CreateShortcut.vbs
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
 echo oLink.TargetPath = "%%windir%%\system32\cmd.exe" >> CreateShortcut.vbs
 echo oLink.Arguments = "/K %MISC_DIR%shell.bat" >> CreateShortcut.vbs
-echo oLink.WorkingDirectory = "%PROJECT_ROOT%" >> CreateShortcut.vbs
+echo oLink.WorkingDirectory = "W:\%PROJECT_NAME%" >> CreateShortcut.vbs
 echo oLink.Save >> CreateShortcut.vbs
 cscript CreateShortcut.vbs
 del CreateShortcut.vbs
